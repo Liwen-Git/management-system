c7b685d8095517f2be950669db326c95bff25687
liwen <liwen1010322212@163.com>
2020-06-18 22:52:45 +0800

    图片上传 过滤相同的图片

--- backend/app/Http/Controllers/Api/UploadController.php
+++ backend/app/Http/Controllers/Api/UploadController.php
@@ -4,21 +4,55 @@ namespace App\Http\Controllers\Api;
 
 
 use App\Result;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class UploadController extends Controller
 {
+    /**
+     * 图片上传
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
     public function uploadImage()
     {
         $file = request()->file('file');
 
-        // 上传
-        $path = $file->store('image/'. auth()->user()->id, 'api');
+        $content = file_get_contents($file->getRealPath());
+        $savePath = 'image/'. auth()->user()->id;
+        $extension = $file->getClientOriginalExtension();
+
+        $nameArr = self::makeName($content, $extension, $savePath);
+        if (!$nameArr['status']) {
+            // 上传
+            $path = $file->storeAs($savePath, $nameArr['name'], 'api');
+        } else {
+            $path = $savePath . '/'. $nameArr['name'];
+        }
 
         $url = asset('api/'. $path);
+        $image = Image::make($url);
 
         return Result::success([
             'path'=> $path,
             'url' => $url,
+            'width' => $image->getWidth(),
+            'height' => $image->getHeight(),
+            'size' => $file->getSize(),
         ]);
+    }
+
+    /**
+     * @param $content  --文件数据流
+     * @param $extension  --文件后缀名
+     * @param string $path --文件存储路径
+     * @return array
+     */
+    public static function makeName($content, $extension, $path = '')
+    {
+        $name = md5($content) . '.'. $extension;
+        $disk = Storage::disk('api');
+        $status = $disk->exists($path. $name);
+
+        return ['status' => $status, 'name' => $name];
     }
 }
